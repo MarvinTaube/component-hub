f92ffe5d6cc5d015511ecba9d6586fb9821cb3ff
marvin <marvin.taube@pigeonport.de>
2026-01-23 09:34:54 +0100

    Initial commit

---
+++ backend/build.gradle.kts
@@ -0,0 +1,31 @@
+val kotlin_version: String by project
+val logback_version: String by project
+val mongo_version: String by project
+
+plugins {
+    kotlin("jvm") version "2.2.21"
+    id("io.ktor.plugin") version "3.3.3"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.21"
+}
+
+group = "de.pigeonport.componentwarehouse"
+version = "0.0.1"
+
+application {
+    mainClass = "io.ktor.server.netty.EngineMain"
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core")
+    implementation("io.ktor:ktor-server-auth")
+    implementation("io.ktor:ktor-server-content-negotiation")
+    implementation("io.ktor:ktor-serialization-kotlinx-json")
+    implementation("org.mongodb:mongodb-driver-core:$mongo_version")
+    implementation("org.mongodb:mongodb-driver-sync:$mongo_version")
+    implementation("org.mongodb:bson:$mongo_version")
+    implementation("io.ktor:ktor-server-netty")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.ktor:ktor-server-config-yaml")
+    testImplementation("io.ktor:ktor-server-test-host")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
